f6509f69ed142b02c528676fbcdabb0465e7da02
kajal-1999-cloud <130256390+kajal-1999-cloud@users.noreply.github.com>
2023-06-23 15:03:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+for(let i=0; i<20 ; i++) {
+	if(i%3==0){
+		alert("fizz")
+	}
+	if(i%5==0){
+		alert("buzz")
+	}
+	if(i%3==0 && i%5==0){
+		alert("fizzbuzz")
+	}
+}
